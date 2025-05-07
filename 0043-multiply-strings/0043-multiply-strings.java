8e0180c942aee38c8fc50695f4bf778193fa78e2
Megha Gupta <155795205+megha2627@users.noreply.github.com>
2025-05-07 11:25:27 +0530

    Time: 9 ms (30.94%), Space: 42.9 MB (27.53%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -0,0 +1,12 @@
+import java.math.BigInteger;
+
+class Solution {
+    public String multiply(String num1, String num2) {
+        BigInteger num11=new BigInteger(num1);
+        BigInteger num22=new BigInteger(num2);
+        BigInteger num3=num11.multiply(num22);
+       
+        return String.valueOf(num3);
+        
+    }
+}
