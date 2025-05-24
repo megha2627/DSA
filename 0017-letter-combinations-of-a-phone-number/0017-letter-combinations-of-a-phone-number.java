08eb6422d3e411302f20ecc0049d4098a369f943
Megha Gupta <155795205+megha2627@users.noreply.github.com>
2025-05-24 15:40:09 +0530

    Time: 1 ms (54.43%), Space: 42.4 MB (26.23%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,8 +1,8 @@
 class Solution {
-    List<String> l;
     public List<String> letterCombinations(String digits) {
-        HashMap<Integer,String> map=new HashMap<>();
-        l=new ArrayList<>();
+        Map<Integer,String> map=new HashMap<>();
+        List<String> l1=new ArrayList<>();
+        l1.add("");
         map.put(2,"abc");
         map.put(3,"def");
         map.put(4,"ghi");
@@ -11,38 +11,25 @@ class Solution {
         map.put(7,"pqrs");
         map.put(8,"tuv");
         map.put(9,"wxyz");
-        
+        if(digits.length()==0){
+            return new ArrayList<String>();
+        }
         for(int i=0;i<digits.length();i++){
-            int p=digits.charAt(i)-'0';
-            String q=map.get(p);
-            int r=map.get(p).length();
-            if(l.isEmpty()){
-                for(int z=0;z<r;z++){
-                    l.add(String.valueOf(q.charAt(z)));
-                }
-
-            }
-            else{
-                solve(q,r,map);
-
-            }
+           String p=map.get(digits.charAt(i)-'0');
+            ArrayList<String> v=new ArrayList<>();
+            l1=solve(p,l1,v);
+        }
+        return l1;
             
-        }
-        return l;
-
-        
     }
-    public void solve(String q,int r,HashMap<Integer,String> map){
-        List<String> x=new ArrayList<>();
-        for(int i=0;i<r;i++){
-            for(int j=0;j<l.size();j++){
-                String c=l.get(j)+String.valueOf(q.charAt(i));
-                x.add(c);
-
+    public List<String> solve(String p,List<String> l1,List<String> v){
+       
+        for(int i=0;i<p.length();i++){
+           
+            for(int j=0;j<l1.size();j++){
+                v.add(l1.get(j)+String.valueOf(p.charAt(i)));
             }
         }
-        l.clear();
-        l.addAll(x);
-
+        return v;
     }
 }
