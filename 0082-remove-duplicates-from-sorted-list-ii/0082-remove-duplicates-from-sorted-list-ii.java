13be48ae46786524d6e72a2e354514c94acb245e
Megha Gupta <155795205+megha2627@users.noreply.github.com>
2025-08-22 17:42:31 +0530

    Time: 0 ms (100%), Space: 43.3 MB (55.54%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
@@ -1,25 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        // Dummy node to handle edge cases like [1,1,1,2,3]
-        ListNode dummy = new ListNode(0, head);
-        ListNode prev = dummy; // Last node before the sublist of duplicates
+        ListNode temp=head;
+        if(temp==null){
+            return null;
 
-        while (head != null) {
-            // Detect the start of a duplicate sublist
-            if (head.next != null && head.val == head.next.val) {
-                // Skip all nodes with the same value
-                while (head.next != null && head.val == head.next.val) {
-                    head = head.next;
-                }
-                // Bypass the entire duplicate block
-                prev.next = head.next;
-            } else {
-                // No duplicate, move prev
-                prev = prev.next;
-            }
-            head = head.next;
         }
-
-        return dummy.next;
+        ListNode prev=null;
+        while(temp!=null && temp.next!=null){
+            if(temp.val==temp.next.val){
+                while(temp!=null && temp.next!=null && temp.val==temp.next.val){
+                    temp=temp.next;
+                }
+                if(prev==null){
+                    head=temp.next;
+                    
+                   
+                    
+                }
+                else{
+                    prev.next=temp.next;
+                    
+                }
+                temp=temp.next;
+            }
+            else{
+                prev=temp;
+                temp=temp.next;
+            }
+        }
+        return head;
+        
     }
 }
